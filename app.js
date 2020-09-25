5358bc5adf555c2c94b865209958e814b30e4e63
cnease10 <cierra.nease10@gmail.com>
2020-09-25 13:45:23 -0700

    added basic js for an accordian

--- app.js
+++ app.js
@@ -189,24 +189,6 @@ for (i = 0; i <buttonAcc.length; i++) {
 }
 
 
-// let i;
-
-// for (i = 0; i < buttonAcc.length; i++) {
-//       buttonAcc[i].addEventListener("click", function() {
-//       /* Toggle between adding and removing the "active" class,
-//       to highlight the button that controls the panel */
-//       this.classList.toggle("active");
-  
-//       /* Toggle between hiding and showing the active panel */
-//       let section = this.nextElementSibling;
-//       if (section.style.display === "block") {
-//         section.style.display = "none";
-//       } else {
-//         section.style.display = "block";
-//       }
-//     });
-//   }
-
 
 //Notes
 // JSON.parse() - parses data that is received as JSON, deserializes a JSON
